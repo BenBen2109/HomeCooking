1235f1dc0ed8ed4cfe92f51efe7b8653dc047f1b
BenBen2109 <72802198+BenBen2109@users.noreply.github.com>
2021-11-08 23:43:03 +0700

    Khánh 8/11, Update profile

--- app/src/main/java/com/cntt/homecooking/SplashScreen.java
+++ app/src/main/java/com/cntt/homecooking/SplashScreen.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.cntt.homecooking.activities.LoginActivity;
+import com.cntt.homecooking.activities.RegisterActivity;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent=new Intent(this,MainActivity.class);
+        Intent intent=new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
