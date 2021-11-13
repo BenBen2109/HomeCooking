466350dd5a418e0462dc6de070b0ed7798887d3a
BenBen2109 <72802198+BenBen2109@users.noreply.github.com>
2021-11-13 22:52:55 +0700

    update layout home, me

--- app/src/main/java/com/cntt/homecooking/SplashScreen.java
+++ app/src/main/java/com/cntt/homecooking/SplashScreen.java
@@ -13,7 +13,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent=new Intent(this, LoginActivity.class);
+        Intent intent=new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
