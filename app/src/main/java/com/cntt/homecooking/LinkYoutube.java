73d09b9e308c201f8fcd9d6fca79bb487b13926f
BenBen2109 <72802198+BenBen2109@users.noreply.github.com>
2021-11-23 22:23:51 +0700

    đổi videoview thành youtubevideoplayer

--- app/src/main/java/com/cntt/homecooking/LinkYoutube.java
+++ app/src/main/java/com/cntt/homecooking/LinkYoutube.java
@@ -13,21 +13,22 @@ public class LinkYoutube extends AppCompatActivity {
 
     private VideoView videoView;
 
-    String videoLink = "https://www.youtube.com/watch?v=Rzs897JoYQA";
+    String videoLink = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_youtube);
+//
+//                          VideoView
+//        videoView = findViewById(R.id.formula_video);
+//        Uri uri = Uri.parse("https://www.youtube.com/watch?v=Rzs897JoYQA");
+//        videoView.setVideoPath("https://www.youtube.com/watch?v=XXPlZJan--s");
+//
+//        MediaController mediaController = new MediaController(this);
+//        mediaController.setMediaPlayer(videoView);
+//        videoView.setMediaController(mediaController);
+//        videoView.start();
 
-        videoView = findViewById(R.id.formula_video);
-        Uri uri = Uri.parse(videoLink);
-        videoView.setVideoURI(uri);
-
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-
-        videoView.setMediaController(mediaController);
-        videoView.start();
     }
 }
