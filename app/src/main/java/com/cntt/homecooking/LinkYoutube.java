288c830bdcf85eedc6ef3da8fb8a68b28b3a4937
Anh Duy <anhduyg07@gmail.com>
2021-11-23 23:34:57 +0700

    LinkYoutube

--- app/src/main/java/com/cntt/homecooking/LinkYoutube.java
+++ app/src/main/java/com/cntt/homecooking/LinkYoutube.java
@@ -1,34 +1,58 @@
 package com.cntt.homecooking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class LinkYoutube extends AppCompatActivity {
+public class LinkYoutube extends YouTubeBaseActivity {
 
-    private VideoView videoView;
+    private YouTubePlayerView youTubePlayerView;
 
-    String videoLink = "";
+    String videoLink = "https://www.youtube.com/embed/ZhYaRAcrUGI";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_youtube);
-//
-//                          VideoView
-//        videoView = findViewById(R.id.formula_video);
-//        Uri uri = Uri.parse("https://www.youtube.com/watch?v=Rzs897JoYQA");
-//        videoView.setVideoPath("https://www.youtube.com/watch?v=XXPlZJan--s");
-//
-//        MediaController mediaController = new MediaController(this);
-//        mediaController.setMediaPlayer(videoView);
-//        videoView.setMediaController(mediaController);
-//        videoView.start();
+        initView();
 
+
+        //Cắt ra id của video youtube
+        String[] link=videoLink.split("embed/");
+        String videoid = link[1];
+
+        //Cấu hình Youtube
+        YouTubePlayer.OnInitializedListener listener=new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                // Chèo id
+                youTubePlayer.loadVideo(videoid);
+                //Tự động play
+                youTubePlayer.play();
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Toast.makeText(LinkYoutube.this, "Có lỗi xảy ra", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        //Truyền Api Key
+        youTubePlayerView.initialize("AIzaSyAN8E7JcTqEIsX8wVLulX3aLxXKH1jqJNQ",listener);
+
+    }
+
+    private void initView() {
+        youTubePlayerView=findViewById(R.id.formula_video);
     }
 }
---
+++ app/src/main/java/com/cntt/homecooking/api/GoogleAPI.java
@@ -0,0 +1,10 @@
+package com.cntt.homecooking.api;
+
+public class GoogleAPI {
+    // Youtube API
+    private static String YOUTUBE_API_KEY="AIzaSyAN8E7JcTqEIsX8wVLulX3aLxXKH1jqJNQ";
+
+    public static String getYoutubeApiKey(){
+        return YOUTUBE_API_KEY;
+    }
+}
