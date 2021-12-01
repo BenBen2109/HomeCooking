9d34b7e480e6fefb529c33faf255c40f1600bbf1
BenBen2109 <72802198+BenBen2109@users.noreply.github.com>
2021-12-02 01:05:54 +0700

    update profile ( thay vị trí setOnClick )

--- app/src/main/java/com/cntt/homecooking/Me.java
+++ app/src/main/java/com/cntt/homecooking/Me.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -30,7 +31,7 @@ public class Me extends Fragment  {
 
     private KhachHang kh;
 
-    private TextView user_info,logOut;
+    private ConstraintLayout user_info,logOut;
     TextView txt1;
 
     private View mView;
