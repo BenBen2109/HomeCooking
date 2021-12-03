25d3e926fd5e90c898ea0e47371cdc9710ea8a1f
BenBen2109 <72802198+BenBen2109@users.noreply.github.com>
2021-12-04 02:31:56 +0700

    Hóa Đơn + LSDH

---
+++ app/src/main/java/com/cntt/homecooking/LichSuHoaDon.java
@@ -0,0 +1,14 @@
+package com.cntt.homecooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LichSuHoaDon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lich_su_hoa_don);
+    }
+}
